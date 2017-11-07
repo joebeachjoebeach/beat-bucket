c46dc377e2bea38d4cf05933019f3cc4683199df
Joe Beach <joebeach@gmail.com>
2017-11-07 16:34:02 -0500

    fix indentation in app component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Tone from 'tone';
 
 const App = () => {
-    return (
-      <div>
-        React + Flask Starter Template
-      </div>
-    );
+  return (
+    <div>
+      React + Flask Starter Template
+    </div>
+  );
 };
 
 export default App;
