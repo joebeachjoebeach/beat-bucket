063fbb0a4c5fab14861b4fbe2769cdbcf7829a61
Joe Beach <joebeach@gmail.com>
2017-11-07 17:38:19 -0500

    update gitignore and fix indentation

--- server.py
+++ server.py
@@ -4,10 +4,7 @@ app = Flask(__name__, static_folder='./dist', template_folder='./dist')
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
-  # this is for running on c9.io:
-  app.run(host='0.0.0.0', port=8080, debug=True)
-  # this is for running in a local dev environment:
-  # app.run(debug=True)
+    app.run(debug=True)
