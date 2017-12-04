6ef78beb598cb69176ea2ddf3b4df0e0bfb9f01e
Joe Beach <joebeach@gmail.com>
2017-12-04 14:52:13 -0500

    extract server.py out to server/ package; add run.py

---
+++ run.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3.6
+
+from server import app
+app.run(debug=True)
---
+++ server/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__, static_folder='../dist', template_folder='../dist')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
