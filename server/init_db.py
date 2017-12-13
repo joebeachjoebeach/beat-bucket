a607cb91d3c5d9389c107888e7665cf2aa00099a
Joe Beach <joebeach@gmail.com>
2017-12-13 09:44:18 -0500

    finish adding projects/tracks tables to init_db.py

--- server/init_db.py
+++ server/init_db.py
@@ -19,15 +19,9 @@ def create_users(cursor):
     )
 
 
-def recreate_users(database_name):
-    '''recreates the users table'''
-    db_conn = connect_to_db(database_name)
-    cursor = db_conn.cursor()
-    drop_users(cursor)
-    create_users(cursor)
-    db_conn.commit()
-    cursor.close()
-    db_conn.close()
+def drop_projects(cursor):
+    '''Drops the projects table'''
+    cursor.execute('DROP TABLE IF EXISTS projects')
 
 
 def create_projects(cursor):
@@ -37,11 +31,16 @@ def create_projects(cursor):
         CREATE TABLE projects (
             id SERIAL PRIMARY KEY,
             name VARCHAR(25) NOT NULL,
-            user_id INTEGER FOREIGN KEY NOT NULL,
+            user_id INTEGER NOT NULL REFERENCES users,
             bpm INTEGER NOT NULL
         )
         '''
     )
+
+
+def drop_tracks(cursor):
+    '''Drops the tracks table'''
+    cursor.execute('DROP TABLE IF EXISTS tracks')
 
 
 def create_tracks(cursor):
@@ -51,7 +50,7 @@ def create_tracks(cursor):
         CREATE TABLE tracks (
             id SERIAL PRIMARY KEY,
             name VARCHAR(20) NOT NULL,
-            project_id INTEGER FOREIGN KEY NOT NULL,
+            project_id INTEGER NOT NULL REFERENCES projects,
             base_note INTEGER,
             muted BOOLEAN,
             soloed BOOLEAN,
@@ -61,6 +60,47 @@ def create_tracks(cursor):
     )
 
 
+def recreate_users(cursor):
+    '''recreates the users table'''
+    drop_users(cursor)
+    create_users(cursor)
+
+
+def recreate_projects(cursor):
+    '''recreates the projects table'''
+    drop_projects(cursor)
+    create_projects(cursor)
+
+
+def recreate_tracks(cursor):
+    '''recreates the projects table'''
+    drop_tracks(cursor)
+    create_tracks(cursor)
+
+
+def recreate_all():
+    '''recreates all the tables'''
+    main_conn = connect_to_db('beatbucket')
+    test_conn = connect_to_db('beatbucket_test')
+
+    main_cur = main_conn.cursor()
+    test_cur = main_conn.cursor()
+
+    recreate_users(main_cur)
+    recreate_users(test_cur)
+    recreate_projects(main_cur)
+    recreate_projects(test_cur)
+    recreate_tracks(main_cur)
+    recreate_tracks(test_cur)
+
+    main_conn.commit()
+    test_conn.commit()
+
+    main_cur.close()
+    test_cur.close()
+    main_conn.close()
+    test_conn.close()
+
+
 if __name__ == '__main__':
-    recreate_users('beatbucket')
-    recreate_users('beatbucket_test')
+    recreate_all()
