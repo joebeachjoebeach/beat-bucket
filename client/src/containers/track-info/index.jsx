73bc3182286ad65f4d6b18e40bbde47f1dca9d92
Joe Beach <joebeach@gmail.com>
2018-01-27 09:29:52 -0500

    change track-info to class component; make track delete button appear only on mouseEnter

--- client/src/containers/track-info/index.jsx
+++ client/src/containers/track-info/index.jsx
@@ -13,52 +13,76 @@ import { deleteTrack } from '../../redux/actions/actions-tracks';
 import { selectTrack } from '../../redux/selectors';
 import './track-info.css';
 
-// convert to class
-// state has { hover: false }
-// handleHover => setState({ hover: true })
-// delete button className={hover ? 'hidden' : ''}
-const TrackInfo = ({
-  name,
-  id,
-  muted,
-  soloed,
-  mute,
-  solo,
-  unmute,
-  unsolo,
-  deleteTrack }) => {
+class TrackInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hover: false };
+    this.handleMuteClick = this.handleMuteClick.bind(this);
+    this.handleSoloClick = this.handleSoloClick.bind(this);
+    this.handleDeleteTrackClick = this.handleDeleteTrackClick.bind(this);
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
+  }
 
-  function handleMuteClick() {
+  handleMuteClick() {
+    const { id, mute, unmute, muted } = this.props;
     muted
       ? unmute(id)
       : mute(id);
   }
 
-  function handleSoloClick() {
+  handleSoloClick() {
+    const { id, solo, unsolo, soloed } = this.props;
     soloed
       ? unsolo(id)
       : solo(id);
   }
 
-  function handleDeleteTrackClick() {
+  handleDeleteTrackClick() {
+    const { id, deleteTrack } = this.props;
     deleteTrack({ trackId: id });
   }
 
-  return (
-    <div className="track-info">
-      <div className="track-info-left">
-        <button onClick={handleDeleteTrackClick}>x</button>
-      </div>
-      <div className="track-info-right">
-        <div className="track-info-title">{name}</div>
-        <div className="track-info-buttons">
-          <button onClick={handleMuteClick} className="track-info-button">m</button>
-          <button onClick={handleSoloClick} className="track-info-button">s</button>
+  handleMouseOver() {
+    this.setState({ hover: true });
+  }
+
+  handleMouseOut() {
+    this.setState({ hover: false });
+  }
+
+  render() {
+    const { name } = this.props;
+    const { hover } = this.state;
+    return (
+      <div className="track-info"
+        onMouseEnter={this.handleMouseOver}
+        onMouseLeave={this.handleMouseOut}
+      >
+        <div className="track-info-left">
+          {hover && <button className="track-info-delete" onClick={this.handleDeleteTrackClick}>x</button>}
+        </div>
+        <div className="track-info-right">
+          <div className="track-info-title">{name}</div>
+          <div className="track-info-buttons">
+            <button
+              onClick={this.handleMuteClick}
+              className="track-info-button"
+            >
+              m
+            </button>
+            <button
+              onClick={this.handleSoloClick}
+              className="track-info-button"
+            >
+              s
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 
 function mapStateToProps(state, ownProps) {
