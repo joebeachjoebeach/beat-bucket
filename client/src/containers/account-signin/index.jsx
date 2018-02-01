38f8eb20085815bbb6b5221d7a164e871ded95e4
Joe Beach <joebeach@gmail.com>
2018-02-01 17:30:19 -0500

    add flask-cors; enable basic account creation in client

--- client/src/containers/account-signin/index.jsx
+++ client/src/containers/account-signin/index.jsx
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 // import { bindActionCreators } from 'redux';
 import './account-signin.css';
 
@@ -18,16 +19,73 @@ const SigninForm = ({ onCreateClick }) => {
   );
 };
 
-const CreateAccountForm = () => {
-  return (
-    <form className="signin-form">
-      <input className="signin-form-item" type="text" placeholder="email address" />
-      <input className="signin-form-item" type="text" placeholder="password" />
-      <input className="signin-form-item" type="text" placeholder="confirm password" />
-      <input className="signin-form-item" type="submit" value="Create Account" />
-    </form>
-  );
-};
+class CreateAccountForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '', confPassword: '' };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleConfPasswordChange = this.handleConfPasswordChange.bind(this);
+    this.handleCreateAccountSubmit = this.handleCreateAccountSubmit.bind(this);
+  }
+
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleConfPasswordChange(event) {
+    this.setState({ confPassword: event.target.value });
+  }
+
+  handleCreateAccountSubmit(event) {
+    const { email, password } = this.state;
+    event.preventDefault();
+    axios.post(
+      'http://127.0.0.1:5000/auth/register',
+      { email, password }
+    )
+      .then(res => { console.log(res); })
+      .catch(e => { console.log(e); });
+  }
+
+  render() {
+    const { email, password, confPassword } = this.state;
+    return (
+      <form className="signin-form" onSubmit={this.handleCreateAccountSubmit}>
+        <input
+          className="signin-form-item"
+          type="email"
+          placeholder="email address"
+          value={email}
+          onChange={this.handleEmailChange}
+        />
+        <input
+          className="signin-form-item"
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={this.handlePasswordChange}
+        />
+        <input
+          className="signin-form-item"
+          type="password"
+          placeholder="confirm password"
+          value={confPassword}
+          onChange={this.handleConfPasswordChange}
+        />
+        <input
+          className="signin-form-item"
+          type="submit"
+          value="Create Account"
+        />
+      </form>
+    );
+  }
+}
 
 class AccountSignin extends Component {
   constructor(props) {
