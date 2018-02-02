6ab99eed3ffe1e64ad6176a5dbff9c5e7a7cbe07
Joe Beach <joebeach@gmail.com>
2018-02-02 16:36:14 -0500

    flesh out initial state and handlers for sign in component

--- client/src/components/signin-form/index.jsx
+++ client/src/components/signin-form/index.jsx
@@ -1,14 +1,46 @@
 // SIGNIN-FORM
 
 import React, { Component } from 'react';
+import axios from 'axios';
 import './signin-form.css';
 
 import InputWithMessage from '../input-with-message';
 
 class SigninForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '' };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSigninSubmit = this.handleSigninSubmit.bind(this);
+  }
 
   componentDidMount() {
     this.emailEl.focus();
+  }
+
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleSigninSubmit(event) {
+    const { email, password } = this.state;
+    event.preventDefault();
+    axios.post(
+      'http://127.0.0.1:5000/auth/login',
+      { email, password }
+    )
+      .then(res => {
+        console.log(res);
+      })
+      .catch(e => {
+        console.log(e);
+        console.log(e.response);
+      });
   }
 
   render() {
@@ -16,16 +48,18 @@ class SigninForm extends Component {
     return (
       <div className="signin-container">
         {message && message}
-        <form className="signin-form">
+        <form className="signin-form" onSubmit={this.handleSigninSubmit}>
           <InputWithMessage
             type="email"
             placeholder="email address"
             inputRef={el => { this.emailEl = el; }}
+            onChange={this.handleEmailChange}
             required
           />
           <InputWithMessage
             type="password"
             placeholder="password"
+            onChange={this.handlePasswordChange}
             required
           />
           <input
